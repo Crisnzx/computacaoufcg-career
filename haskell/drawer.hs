a2636bb0f58bb052bfde2401b668dcfb9aa7629b
Crisnzx <cristovaon13@gmail.com>
2023-04-27 09:47:23 -0300

    add alphabet sprites

--- haskell/drawer.hs
+++ haskell/drawer.hs
@@ -1,15 +1,20 @@
-module Drawer (buildLine) where
+module Drawer (makeLines) where
+import Sprites
 
-buildLine:: [String] -> [String]
-buildLine sprites = concatLines (map (\sprite -> lines sprite) sprites)
+makeLines:: String -> [Char] -> String -> [String]
+makeLines mapper dataList spacer
+  -- | mapper == "card" =
+  --   let sprites = map (\cardNumber -> getSpriteCard cardNumber) dataList
+  --   in concatLines (map (\sprite -> lines sprite) sprites) 0 spacer
+  | mapper == "char" =
+    let sprites = map (\character -> getSpriteChar character) dataList
+    in (concatLines (map (\sprite -> lines sprite) sprites) 0 spacer)
 
-concatLines:: [[String]] -> Int -> [String]
-concatLines sprites i = 
-  | i < length (concatLine sprites i ++ concatLines sprites i+1) = concatLine sprites i ++ concatLines sprites i+1
+concatLines:: [[String]] -> Int -> String -> [String]
+concatLines sprites lineNumber spacer
+  | lineNumber < length (sprites !! 0) = [concatLine sprites lineNumber spacer] ++ concatLines sprites (lineNumber+1) spacer
   | otherwise = []
 
-concatLine:: [[String]] -> [String]
-concatLine [] i = "\n"
-concatLine (h: t) lineNumber = h !! lineNumber ++ concatLine t
-
-
+concatLine:: [[String]] -> Int -> String -> String
+concatLine (h: []) lineNumber spacer = h !! lineNumber ++ "\n"
+concatLine (h: t) lineNumber spacer = h !! lineNumber ++ spacer ++ concatLine t lineNumber spacer
--- haskell/main.hs
+++ haskell/main.hs
@@ -1,44 +1,7 @@
--- import Menu
--- import Cards
-
-getASCIICard2:: String
-
-getASCIICard1 = "░░one█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard2 = "░░two█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard3 = "░░thr█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard4 = "░░fou█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard5 = "░░fiv█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard6 = "░░six█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard7 = "░░sev█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-getASCIICard8 = "░░eig█████████░░\n██░░░░░░░░░░░░██\n██░█▀▀█░░█▀▀█░██\n██░█▄▄█░░█▄▄█░██\n██░▄▄▄█░░▄▄▄█░██\n██░░░░░░░░░░░░██\n██░░░░░░░░░░░░██\n░░████████████░░\n"
-
-mapNumberToAscii:: Int -> String
-mapNumberToAscii 1 = getASCIICard1
-mapNumberToAscii 2 = getASCIICard2
-mapNumberToAscii 3 = getASCIICard3
-mapNumberToAscii 4 = getASCIICard4
-mapNumberToAscii 5 = getASCIICard5
-mapNumberToAscii 6 = getASCIICard6
-mapNumberToAscii 7 = getASCIICard7
-mapNumberToAscii 8 = getASCIICard8
-
-buildColumns:: [String] -> String -> [String]
-buildColumns sprites spacer = concatLines (map (\sprite -> lines sprite) sprites) 0 spacer
-
-concatLines:: [[String]] -> Int -> String -> [String]
-concatLines sprites i spacer
-  | i < length (sprites !! 0) = [concatLine sprites i spacer] ++ concatLines sprites (i+1) spacer
-  | otherwise = []
-
-concatLine:: [[String]] -> Int -> String -> String
-concatLine (h: []) lineNumber spacer = h !! lineNumber ++ "\n"
-concatLine (h: t) lineNumber spacer = h !! lineNumber ++ spacer ++ concatLine t lineNumber spacer
-
-
+import Menu
+import Drawer
 
 main:: IO()
 main = do
   -- printMenu
-  let drawnCards = [5,3,1,8]
-  let asciiDrawnCards = map (\cardNumber -> mapNumberToAscii cardNumber) drawnCards
-  mapM_ putStr (buildColumns asciiDrawnCards "░░░░░░")
+  mapM_ putStr (makeLines "char" "cristovao kenned e ismael lindos" "  ")
