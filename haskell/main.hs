ff2e831720e6e23ba2681cfa2e1bb847b2d62714
Crisnzx <cristovaon13@gmail.com>
2023-05-06 11:26:26 -0300

    clear the mess

--- haskell/main.hs
+++ haskell/main.hs
@@ -3,21 +3,13 @@ import Drawer
 
 main:: IO()
 main = do
-  -- mapM_ putStr (makeTextScreen ["prepare-se. o sapato eh amalero!?"])
-  mapM_ putStr (makeTextScreen [
-      "parabens!",
-      "aproveite ao maximo a toda experiencia proporcionada no curso!",
-      "voce se tornara um grande cientista da computacao?",
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    ])
+  printMenu
   putStrLn ""
-  mapM_ putStr (makeBattlefield 70 4 "plpBoss" [2, 13, 9, 1, 6] [13, 9])
+  -- mapM_ putStr (makeBattlefield 70 4 "plpBoss" [2, 13, 9, 1, 6] [13, 9])
 
-
--- corrigir as sprites de numero
 
 -- o main mostrara a sequencia da historia do jogo
--- printmenu
+-- printmenu (EM DESENVOLVIMENTO)
 -- fubicaStory
 -- fubicaBattle
 -- daltonStory
