7442969ac483ee8ef48584a10b228b4453d6108e
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2023-06-09 16:05:36 -0300

    Prolog transition menu and IOHELPERS

--- haskell/IOHelpers.hs
+++ haskell/IOHelpers.hs
@@ -1,22 +1,22 @@
 module IOHelpers (clearScreen, onEnterContinue, printTextScreen, delay) where
+
+import Control.Concurrent (threadDelay)
 import Drawer
 import System.Process (callCommand)
-import Control.Concurrent (threadDelay)
 
 clearScreen :: IO ()
 clearScreen = callCommand "clear"
 
-printTextScreen:: [String] -> IO()
+printTextScreen :: [String] -> IO ()
 printTextScreen texts = do
   clearScreen
   mapM_ putStr (drawTextScreen texts)
 
-onEnterContinue:: IO()
+onEnterContinue :: IO ()
 onEnterContinue = do
   putStrLn " \n=== Pressione enter para continuar. ==="
-  line <- getLine:: IO String
+  line <- getLine :: IO String
   putStrLn "\n\n"
 
-
-delay:: IO()
+delay :: IO ()
 delay = threadDelay (1 * 1000000) -- 1.0 segundo
--- prolog/IOHelpers.pl
+++ prolog/IOHelpers.pl
@@ -1,7 +1,19 @@
 :- use_module('./Helpers.pl').
 
+clearScreen :-
+    system('clear').
+
+onEnterContinue :-
+  writeln(" \n=== Pressione enter para continuar. ==="),
+  read_line(_),
+  writeln("\n\n").
+
 printStringList([]).
 printStringList([Head|Tail]) :-
   write(Head),
   write("\n"),
   printStringList(Tail).
+
+
+delay :-
+    sleep(1).
