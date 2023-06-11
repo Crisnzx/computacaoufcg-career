d8577a4752ed8e840e6581e6e2ab043c0aef4ebe
kenbme <guilhermekenned1@gmail.com>
2023-06-11 15:24:37 -0300

    sorteando cartas

--- prolog/Battle.pl
+++ prolog/Battle.pl
@@ -1,7 +1,20 @@
-:- module(Battle, [battle/7]).
+:- module(Battle, [battle/5]).
 :- use_module('./Drawer.pl').
 :- use_module('./IOHelpers.pl').
 :- use_module('./Helpers.pl').
+
+sortear_lista_aleatoria(Tamanho, ValorMaximo, ListaSorteada) :-
+    sortear_lista_aleatoria(Tamanho, ValorMaximo, [], ListaSorteada).
+
+sortear_lista_aleatoria(0, _, ListaSorteada, ListaSorteada) :- !.
+sortear_lista_aleatoria(Tamanho, ValorMaximo, ListaParcial, ListaSorteada) :-
+    random(1, ValorMaximo, NumeroSorteado),
+    NovoTamanho is Tamanho - 1,
+    sortear_lista_aleatoria(NovoTamanho, ValorMaximo, [NumeroSorteado|ListaParcial], ListaSorteada).
+    
+battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, Difficulty) :-
+    sortear_lista_aleatoria(5, 12, NewCards),
+    battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, NewCards, [0, 0], Difficulty).
 
 battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, PlayerCards, CurrentCards, Difficulty) :-
     % Pegando Dificuldade
@@ -37,9 +50,9 @@ battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, PlayerCards, Curren
     (PlayerChoice < BossChoice , BossEnergy - 15 > -1, BossLifeNew is BossLife, PlayerLifeNew is PlayerLife - 10);
     (PlayerChoice = BossChoice, BossLifeNew is BossLife - 10, PlayerLifeNew is PlayerLife - 10)),
 
-    
+    sortear_lista_aleatoria(5, 12, NewCards),
     % Continua batalha ou encerra
     PlayerLifeNew > 0,
     BossLifeNew > 0,
-    battle(PlayerColors, [PlayerLifeNew, PlayerEnergyNew], [BossLifeNew, BossEnergyNew], Boss, PlayerCards, [0, 0], Difficulty);
+    battle(PlayerColors, [PlayerLifeNew, PlayerEnergyNew], [BossLifeNew, BossEnergyNew], Boss, NewCards, [0, 0], Difficulty);
     true.
