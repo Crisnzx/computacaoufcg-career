34c1e22cddd1b1d0af2363af767f5369553e0fa9
kenbme <guilhermekenned1@gmail.com>
2023-06-11 14:51:22 -0300

    energia

--- prolog/Battle.pl
+++ prolog/Battle.pl
@@ -14,7 +14,7 @@ battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, PlayerCards, Curren
     getElementByIndex(BossAttributes, 1, BossEnergy),
 
     drawBattlefield(PlayerColors, PlayerAttributes, BossAttributes, 
-        Boss, PlayerCards, CurrentCards, Result0), clearScreen(), write(Result0),
+        Boss, PlayerCards, CurrentCards, Result0), write(Result0),
 
     write("Escolha uma carta de 0-4\n"),
     read(PlayerNumber),
@@ -22,21 +22,24 @@ battle(PlayerColors, PlayerAttributes, BossAttributes, Boss, PlayerCards, Curren
     random_between(D0, D1, BossChoice),
 
     % Desenhando escolha
-    drawBattlefield(PlayerColors, [PlayerLife, PlayerEnergy], [PlayerLife, PlayerEnergy], 
-    Boss, [1, 2, 3, 4, 5], [PlayerChoice, BossChoice], Result1), 
-    clearScreen(), write(Result1),
+    drawBattlefield(PlayerColors, PlayerAttributes, BossAttributes, 
+    Boss, PlayerCards, [PlayerChoice, BossChoice], Result1), 
+    write(Result1),
     delay(),
 
-    % Verificando energia suficiente TODO
-    
+
+    % Verificando energia suficiente
+    (PlayerChoice > 0, PlayerEnergy - 15 > -1, PlayerEnergyNew is PlayerEnergy - 15; PlayerEnergyNew is PlayerEnergy + 25),
+    (BossChoice > 0, BossEnergy - 15 > -1, BossEnergyNew is BossEnergy - 15; BossEnergyNew is BossEnergy + 25),
 
     % Definindo quem leva dano
-    (PlayerChoice > BossChoice -> 
-        BossLifeNew is BossLife - 10, PlayerLifeNew = PlayerLife;
-        PlayerLifeNew is PlayerLife - 10, BossLifeNew = BossLife ),
+    ((PlayerChoice > BossChoice, PlayerEnergy - 15 > -1, PlayerLifeNew is PlayerLife, BossLifeNew is BossLife - 10);
+    (PlayerChoice < BossChoice , BossEnergy - 15 > -1, BossLifeNew is BossLife, PlayerLifeNew is PlayerLife - 10);
+    (PlayerChoice = BossChoice, BossLifeNew is BossLife - 10, PlayerLifeNew is PlayerLife - 10)),
 
+    
     % Continua batalha ou encerra
     PlayerLifeNew > 0,
     BossLifeNew > 0,
-    battle(PlayerColors, [PlayerLifeNew, PlayerEnergy], [BossLifeNew, BossEnergy], Boss, PlayerCards, [0, 0], Difficulty);
+    battle(PlayerColors, [PlayerLifeNew, PlayerEnergyNew], [BossLifeNew, BossEnergyNew], Boss, PlayerCards, [0, 0], Difficulty);
     true.
--- prolog/main.pl
+++ prolog/main.pl
@@ -6,13 +6,13 @@
 :- use_module('./IOHelpers.pl').
 
 main :-
+  battle(["red", "magenta", "green", "yellow", "blue"], [70, 10], [10, 100], 
+    "admBoss", [1, 2, 3, 4, 5], [0, 0], [1, 5]),
   battle(["red", "magenta", "green", "yellow", "blue"], [70, 20], [90, 50], 
-    "admBoss", [1, 2, 3, 4, 5], [6, 7], [1, 5]),
+    "p1Boss", [1, 2, 3, 4, 5], [0, 0], [3, 7]),
   battle(["red", "magenta", "green", "yellow", "blue"], [70, 20], [90, 50], 
-    "p1Boss", [1, 2, 3, 4, 5], [6, 7], [3, 7]),
+      "p2Boss", [1, 2, 3, 4, 5], [0, 0], [3, 8]),
   battle(["red", "magenta", "green", "yellow", "blue"], [70, 20], [90, 50], 
-      "p2Boss", [1, 2, 3, 4, 5], [6, 7], [3, 8]),
+    "edaBoss", [1, 2, 3, 4, 5], [0, 0], [4, 10]),
   battle(["red", "magenta", "green", "yellow", "blue"], [70, 20], [90, 50], 
-    "edaBoss", [1, 2, 3, 4, 5], [6, 7], [4, 10]),
-  battle(["red", "magenta", "green", "yellow", "blue"], [70, 20], [90, 50], 
-      "plpBoss", [1, 2, 3, 4, 5], [6, 7], [6, 10]).
+      "plpBoss", [1, 2, 3, 4, 5], [0, 0], [6, 10]).
